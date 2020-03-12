334e4e282f626b24103f2ee142b476989e3f2594
Maeva Pasteur <maeva.pasteur@hetic.net>
2020-03-12 10:12:01 +0100

    V1

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,27 +16,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Déclaration pour la page d'accueil
     const searchData = document.querySelector('[name="searchData"]');
-    const themoviedbUrl = 'https://api.themoviedb.org/3/search/movie?api_key=6fd32a8aef5f85cabc50cbec6a47f92f&query=';
     const favoriteSection = document.querySelector(".section-favorites");
     const favoriteContainer = document.querySelector(".section-favorites .container");
     const moviePopin = document.querySelector('#moviePopin article');
     const movieList = document.querySelector('#movieList');
+    const genreList = document.querySelector('.section-genres');
     let userId, favorites = [];
 
 
     /*******************************************************************************************************************
      * Déclaration des fonctions
      *******************************************************************************************************************/
-
-    /**
-     * Vérifier si l'utilisateur est connecté
-     * @returns {boolean}
-     */
-    const checkLastSession = () => {
-        const lastSessionObject = sessionStorage.getItem("appFilm");
-        const lastSession = JSON.parse(lastSessionObject);
-        return !!lastSession
-    };
 
     /**
      * Tester l'inscription de l'utilisateur
@@ -97,6 +87,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }).then( jsonData => {
                 if(jsonData) {
                     updateLocalStorage(jsonData.data.identity['_id'], email, pw, jsonData.data.identity['pseudo']);
+                    console.log(jsonData);
                     window.location.href = 'index.html';
                 }
             })
@@ -138,116 +129,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
-
-    /**
-     * Créer un élement film qui sera affiché dans le DOM
-     * @param movie
-     * @param isFavorite
-     * @returns {string}
-     */
-    const createMovieItem = (movie, isFavorite) => {
-        let div = `
-                    <article>
-                        <figure>
-                            <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" alt="${movie.title}">
-                            <figcaption movie-id="${movie.id}">
-                                <h3>${movie.original_title}</h3> 
-                                <p>Voir plus...</p>
-                            </figcaption>
-                        </figure>
-                        <div data-favorite="${isFavorite}" data-title="${movie.title}" data-id="${movie.id}">
-                            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
-                                <path d="M376,30c-27.783,0-53.255,8.804-75.707,26.168c-21.525,16.647-35.856,37.85-44.293,53.268
-                                \t\t\tc-8.437-15.419-22.768-36.621-44.293-53.268C189.255,38.804,163.783,30,136,30C58.468,30,0,93.417,0,177.514
-                                \t\t\tc0,90.854,72.943,153.015,183.369,247.118c18.752,15.981,40.007,34.095,62.099,53.414C248.38,480.596,252.12,482,256,482
-                                \t\t\ts7.62-1.404,10.532-3.953c22.094-19.322,43.348-37.435,62.111-53.425C439.057,330.529,512,268.368,512,177.514
-                                \t\t\tC512,93.417,453.532,30,376,30z"/>
-                            </svg>
-                        </div>
-                    </article>
-                `;
-        return div;
-    };
-
-
-    /**
-     * Obtenir la liste des genres du film affiché puis les ajouter dans le DOM de sa popin
-     * @param idMovie
-     * @param el = popin
-     */
-    const getMovieGenre = (idMovie, el) => {
-        let genres = '';
-        fetch( `https://api.themoviedb.org/3/movie/${idMovie}?api_key=6fd32a8aef5f85cabc50cbec6a47f92f`)
-            .then( response => response.ok ? response.json() : 'Response not OK' )
-            .then( jsonData => {
-                if(jsonData.genres.length) {
-                    jsonData.genres.forEach(genre => {
-                        genres += `<a href="#" data-genre="${genre.id}">${genre.name}</a>`;
-                    });
-                    let genresEl = document.createElement('div');
-                    genresEl.classList.add('list-genres');
-                    genresEl.innerHTML = genres;
-                    el.querySelector('ul').parentNode.insertBefore(genresEl, el.querySelector('ul'));
-                    el.querySelectorAll('[data-genre]').forEach(genre => {
-                        let id = Number(genre.getAttribute('data-genre'));
-                        genre.addEventListener('click', e => {
-                            e.preventDefault();
-                            closePopin();
-                            fetchGenre(id);
-                        })
-                    })
-                }
-            })
-            .catch( err => console.error(err) );
-    };
-
-    /**
-     * Obtenir la liste des films par genre
-     * @param id = id du genre
-     */
-    const fetchGenre = id => {
-        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=6fd32a8aef5f85cabc50cbec6a47f92f&with_genres=${id}`)
-            .then( response => response.ok ? response.json() : 'Response not OK' )
-            .then( jsonData => {
-                displayMovieList(jsonData.results)
-            })
-            .catch( err => console.error(err) );
-    };
-
-    /**
-     * Ajouter un film aux favoris
-     * @param btn
-     */
-    const addToFavorite = (btn) => {
-        const data = {
-            author: userId,
-            id: btn.dataset.id,
-            name: btn.dataset.title
-        };
-        fetch( 'https://api.dwsapp.io/api/favorite', {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {'Content-Type': 'application/json'}
-        }).then( apiResponse => {
-            if( apiResponse.ok ){
-                return apiResponse.json();
-            }
-        }).then( jsonData => {
-            if(jsonData) {
-                console.log(jsonData);
-                console.log(jsonData.data.data.id);
-                createFavoriteElement(jsonData.data.data.id, 'true');
-                btn.setAttribute('data-favorite', 'true');
-            }
-        }).catch( apiError => {
-                console.error(apiError);
-        });
-    };
-
-    /**
-     * Afficher la liste des films favoris
-     */
-    const showFavorites = () => {
+    const getFavorites = () => {
         fetch(`https://api.dwsapp.io/api/me/${userId}`)
             .then( response => response.ok ? response.json() : 'Response not OK' )
             .then( jsonData => {
@@ -258,27 +140,11 @@ document.addEventListener('DOMContentLoaded', () => {
                     let id = item.id;
                     if(!favorites.includes(id)) {
                         favorites.push(id);
-                        createFavoriteElement(id)
+                        getMovieData(id).then((movieData) => {
+                            favoriteContainer.innerHTML += displayMovie(movieData.id, movieData.title, movieData.poster_path, true)
+                        });
                     }
                 });
-            })
-            .catch( err => console.error(err) );
-    };
-
-    /**
-     * Ajouter un film à la liste des films favoris affichés
-     * @param idMovie
-     */
-    const createFavoriteElement = (idMovie) => {
-        fetch( `https://api.themoviedb.org/3/movie/${idMovie}?api_key=6fd32a8aef5f85cabc50cbec6a47f92f`)
-            .then( response => response.ok ? response.json() : 'Response not OK' )
-            .then( jsonData => {
-                if(jsonData) {
-                    favoriteContainer.innerHTML += createMovieItem(jsonData, 'true');
-                    document.querySelector(`figcaption[movie-id="${jsonData.id}"]`).addEventListener('click', () => {
-                        getMovieList(id);
-                    })
-                }
             })
             .catch( err => console.error(err) );
     };
@@ -295,107 +161,14 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     /**
-     * Obtenir la valeur renseignée dans le champs de recherche
+     * Vérifier si l'utilisateur est connecté
+     * @returns {boolean}
      */
-    const getSearchSubmit = () => {
-        document.querySelector('.form-search').addEventListener('submit', ev => {
-            ev.preventDefault();
-            searchData.value.length > 0
-                ? getMovieList(searchData.value)
-                : console.log('Minimum 1 caractère');
-        })
+    const checkLastSession = () => {
+        const lastSessionObject = sessionStorage.getItem("appFilm");
+        const lastSession = JSON.parse(lastSessionObject);
+        return !!lastSession
     };
-
-    /**
-     * Afficher les films dans le DOM
-     * @param collection = liste des films à afficher
-     */
-    const displayMovieList = collection => {
-        searchData.value = '';
-        movieList.innerHTML = '';
-        for( let i = 0; i < collection.length; i++ ){
-            let isFavorite = favorites.includes(String(collection[i].id));
-            movieList.innerHTML += createMovieItem(collection[i], isFavorite);
-        }
-        document.querySelectorAll('figcaption').forEach(item => {
-            item.addEventListener('click', () => {
-                getMovieList( +item.getAttribute('movie-id') );
-            })
-        });
-        document.querySelectorAll('[data-favorite]').forEach(btn => {
-            btn.addEventListener('click', () => {
-                if(!favorites.includes(btn.dataset.id)) {
-                    favorites.push(btn.dataset.id);
-                    addToFavorite(btn);
-                }
-            })
-        });
-    };
-
-    /**
-     * Obtenir les films par mot clé et page
-     * @param keywords
-     * @param index
-     */
-    const getMovieList = (keywords, index = 1) => {
-        let fetchUrl = null;
-        typeof keywords === 'number'
-            ? fetchUrl = `https://api.themoviedb.org/3/movie/${keywords}?api_key=6fd32a8aef5f85cabc50cbec6a47f92f`
-            : fetchUrl = themoviedbUrl + keywords + '&page=' + index;
-        fetch( fetchUrl )
-            .then( response => response.ok ? response.json() : 'Response not OK' )
-            .then( jsonData => {
-                typeof keywords === 'number'
-                    ? displayPopin(jsonData)
-                    : displayMovieList(jsonData.results)
-            })
-            .catch( err => console.error(err) );
-    };
-
-    /**
-     * Mettre à jour le contenu de la popin du film
-     * @param data = objet film
-     */
-    const displayPopin = (data) => {
-        moviePopin.innerHTML = `
-                <div>
-                    <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="${data.original_title}">
-                </div>
-                <div>
-                    <h2>${data.original_title}</h2>
-                    <ul>
-                        <li><span>Note : </span>${data.vote_average}/10</li>
-                        <li><span>Popularité : </span>${data.popularity}</li>
-                        <li><span>Sortie : </span>${data.release_date}</li>
-                    </ul>
-                    <p>${data.overview}</p>
-                    <button>Voir en streaming</button>
-                    <button id="closeButton">Close</button>
-                </div>
-            `;
-        moviePopin.parentElement.classList.add('open');
-        document.querySelector('#closeButton').addEventListener('click', () => {
-            closePopin();
-        });
-        getMovieGenre(data.id, moviePopin);
-    };
-    /**
-     * Fermer la popin des détails du film
-     */
-    const closePopin = () => {
-        let button = document.querySelector('#closeButton');
-        button.parentElement.parentElement.parentElement.classList.add('close');
-        setTimeout( () => {
-            button.parentElement.parentElement.parentElement.classList.remove('open');
-            button.parentElement.parentElement.parentElement.classList.remove('close');
-        }, 500 )
-    };
-
-
-
-    /*******************************************************************************************************************
-     * NEW
-     *******************************************************************************************************************/
 
     const getMovieData = (id) => {
         return new Promise(resolve => {
@@ -408,17 +181,17 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
-    const initPopin = async (movieId) => {
-        let data = await getMovieData(movieId);
-        let genres = '';
-        if(data.genres.length) {
-            genres += '<div class="list-genres">';
-            data.genres.forEach(genre => {
-                genres += `<a href="#" data-genre="${genre.id}">${genre.name}</a>`;
-            });
-            genres += '</div>';
-        }
-        moviePopin.innerHTML = `
+    const initPopin = movieId => {
+        getMovieData(movieId).then(data => {
+            let genres = '';
+            if(data.genres.length) {
+                genres += '<div class="list-genres">';
+                data.genres.forEach(genre => {
+                    genres += `<a href="#" data-genre="${genre.id}">${genre.name}</a>`;
+                });
+                genres += '</div>';
+            }
+            moviePopin.innerHTML = `
                 <div>
                     <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="${data.original_title}">
                 </div>
@@ -435,9 +208,10 @@ document.addEventListener('DOMContentLoaded', () => {
                     <button id="closeButton">Close</button>
                 </div>
             `;
-        moviePopin.parentElement.classList.add('open');
-        document.querySelector('#closeButton').addEventListener('click', (e) => {
-            moviePopin.parentElement.classList.remove('open');
+            moviePopin.parentElement.classList.add('open');
+            document.querySelector('#closeButton').addEventListener('click', (e) => {
+                moviePopin.parentElement.classList.remove('open');
+            });
         });
     };
 
@@ -456,6 +230,12 @@ document.addEventListener('DOMContentLoaded', () => {
             movieList.innerHTML = '';
             moviePopin.parentElement.classList.remove('open');
             this.getMovies();
+            this.scrollToList();
+        }
+        scrollToList() {
+            movieList.scrollIntoView({
+                behavior: 'smooth'
+            });
         }
         getMovies() {
             fetch(this.url)
@@ -467,18 +247,17 @@ document.addEventListener('DOMContentLoaded', () => {
                     });
                     document.querySelectorAll('[data-favorite="false"]').forEach(favoriteBtn => {
                         favoriteBtn.addEventListener('click', () => {
-                            addToFavorite(favoriteBtn);
+                            if(!favorites.includes(favoriteBtn.dataset.id)) {
+                                this.addToFavorites(favoriteBtn);
+                                favorites.push(favoriteBtn.dataset.id)
+                            }
                         })
                     });
-                    document.querySelectorAll('figcaption').forEach(btn => {
-                        btn.addEventListener('click', () => {
-                            initPopin(btn.dataset.id);
-                        })
-                    })
                 })
                 .catch( err => console.error(err) );
         }
         addToFavorites(btn) {
+            favoriteSection.classList.remove('hide');
             const data = {
                 author: userId,
                 id: btn.dataset.id,
@@ -492,12 +271,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 if( apiResponse.ok ) return apiResponse.json();
             }).then( jsonData => {
                 if(jsonData) {
-                    console.log(jsonData);
-                    console.log(jsonData.data.data.id);
                     btn.setAttribute('data-favorite', 'true');
-                    /*
-                    createFavoriteElement(jsonData.data.data.id, 'true');
-                     */
+                    getMovieData(data.id).then(movieData => {
+                        favoriteContainer.innerHTML += displayMovie(movieData.id, movieData.title, movieData.poster_path, true)
+                    });
                 }
             }).catch( apiError => {
                 console.error(apiError);
@@ -528,6 +305,19 @@ document.addEventListener('DOMContentLoaded', () => {
             `
     };
 
+    /**
+     * Display a link for every genre's movies
+     */
+    const getGenresList = () => {
+        fetch( 'https://api.themoviedb.org/3/genre/movie/list?api_key=6fd32a8aef5f85cabc50cbec6a47f92f')
+            .then( response => response.ok ? response.json() : 'Response not OK' )
+            .then( jsonData => {
+                jsonData.genres.forEach(genre => {
+                    genreList.innerHTML += `<a href="#" data-genre="${genre.id}">${genre.name}</a>`
+                })
+            })
+            .catch( err => console.error(err) );
+    };
 
 
 
@@ -553,14 +343,17 @@ document.addEventListener('DOMContentLoaded', () => {
             window.location.href = 'identification.html';
         } else {
             window.addEventListener('click', e => {
-                if(e.target.hasAttribute('data-genre')) {
+                if (e.target.hasAttribute('data-genre')) {
                     e.preventDefault();
                     new MovieList(`https://api.themoviedb.org/3/discover/movie?api_key=6fd32a8aef5f85cabc50cbec6a47f92f&with_genres=${e.target.dataset.genre}`);
+                } else if(e.target.closest('figcaption')) {
+                    initPopin(e.target.closest('figcaption').dataset.id)
                 }
             });
             updateAccountInfos();
+            getGenresList();
             initSearchForm();
-            showFavorites();
+            getFavorites();
         }
     }
 
